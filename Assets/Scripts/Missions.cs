81336c0dfc61a1bbb2dd12ed165bc286a93ad7d5
Duck <11778864+ducksplash@users.noreply.github.com>
2020-11-23 01:55:14 +0000

    cat launcher
    made the cannon work. struggling with making winston blast off.

--- Assets/Scripts/Missions.cs
+++ Assets/Scripts/Missions.cs
@@ -52,7 +52,7 @@ public class Missions : MonoBehaviour
 					MissionText.text = NewText;			
 
 				}
-			
+							
 			}
 	}	
 }
--- Assets/Scripts/launchpad.cs
+++ Assets/Scripts/launchpad.cs
@@ -10,14 +10,22 @@ public class launchpad : MonoBehaviour
 //public Text buildText;
 public GameObject launchpadPlate;
 public GameObject launchPadGUI;
+public GameObject RegularCamera;
+public GameObject cannoncam;
+public GameObject cannonSeat;
 public GameObject MoonCannon;
 public GameObject Collectables;
+private int speed = 50;
 public GameObject pressF;
+public GameObject xToLaunch;
+public GameObject TargetRock;
+public GameObject fToGetIn;
 public GameObject Player;
 private string NewText;
 private int partsFound;
 private float detectionRange;
-
+private bool readyForLaunch = false;
+private bool launchingNow = false;
 
 
 
@@ -29,7 +37,15 @@ private float detectionRange;
         launchpadPlate = GameObject.FindWithTag("launchpadPlate");
         launchPadGUI = GameObject.FindWithTag("launchPadGUI");
         Collectables = GameObject.FindWithTag("Collectables");
+        cannonSeat = GameObject.FindWithTag("cannonseat");
+        cannoncam = GameObject.FindWithTag("cannoncamera");
+        RegularCamera = GameObject.FindWithTag("MainCamera");
         pressF = GameObject.FindWithTag("pressF");
+        xToLaunch = GameObject.FindWithTag("XtoLaunch");
+        fToGetIn = GameObject.FindWithTag("fToGetIn");
+        TargetRock = GameObject.FindWithTag("TargetRock");
+
+		
     }
  
  
@@ -44,12 +60,14 @@ private float detectionRange;
 				launchPadGUI.GetComponent<CanvasGroup>().alpha = 1.0f;
 				
 				
-				if (Input.GetButton("USE") && partsFound == 5)
+				if (Input.GetKeyDown("f") && partsFound == 5)
 				{
 					NewText = "That's not quite right but it'll have to do;\nthere's no time for a re-do. Get in.";
 					MoonCannon.SetActive(true);
 					pressF.GetComponent<CanvasGroup>().alpha = 0.0f;	
-					launchPadGUI.GetComponentInChildren<Text>().text = NewText;						
+					fToGetIn.GetComponent<CanvasGroup>().alpha = 1.0f;	
+					launchPadGUI.GetComponentInChildren<Text>().text = NewText;	
+					
 				}
 
 			}
@@ -57,5 +75,44 @@ private float detectionRange;
 			{
 				launchPadGUI.GetComponent<CanvasGroup>().alpha = 0.0f;			
 			}
-	}
+			
+			if (detectionRange < 30 && partsFound == 5 && MoonCannon.activeSelf && Input.GetKeyUp("f"))
+				{
+					readyForLaunch = true;
+				}
+				
+			
+			
+			if (detectionRange < 30 && MoonCannon.activeSelf && readyForLaunch)
+			{	
+				if (Input.GetKeyDown("f"))
+				{
+				
+				Player.transform.position = cannonSeat.transform.position;
+				cannoncam.SetActive(true);				
+				RegularCamera.SetActive(false);
+				
+				fToGetIn.GetComponent<CanvasGroup>().alpha = 0.0f;	
+				xToLaunch.GetComponent<CanvasGroup>().alpha = 1.0f;
+				
+				NewText = "Strap in lads, it's happening tonight.";
+				launchPadGUI.GetComponentInChildren<Text>().text = NewText;	
+				launchingNow = true;
+				}
+			}			
+			
+
+				
+			if (detectionRange < 30 && MoonCannon.activeSelf && launchingNow)
+			{	
+				if (Input.GetKeyDown("x"))
+				{
+				
+
+					
+				//UnityEngine.SceneManagement.SceneManager.LoadScene("HomeBound");				
+				}
+			}
+		}
+
 }
